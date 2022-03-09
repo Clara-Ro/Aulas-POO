f8b1351ff82d6822624d1416e04e44798624c825
Clara-Ro <lara.rhee.gree@gmail.com>
2022-03-09 10:38:22 -0300

    calculo do salário

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log ('teste')
+let salario = 6000
+let porcentagem = 25/100
+let novoSalario = salario + (salario * porcentagem)
+
+console.log(`valor do salário: ${salario}, 
+taxa de aumento: ${porcentagem*100}%, novo salário: ${novoSalario}`)
